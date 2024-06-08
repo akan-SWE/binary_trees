1d3916449551f8847e361dd1fe5bb01d0a9216b6
akanSWE <a.akanSWE@gamil.com>
2024-06-08 09:12:51 +0100

    fix: betty errors.

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -7,13 +7,14 @@
  *
  * Return: void
  */
-void binary_tree_delete(binary_tree_t *tree) {
-  /* base case */
-  if (tree == NULL)
-    return;
+void binary_tree_delete(binary_tree_t *tree)
+{
+	/* base case */
+	if (tree == NULL)
+		return;
 
-  /* recursive call */
-  binary_tree_delete(tree->left);
-  binary_tree_delete(tree->right);
-  free(tree); /* delete each node on return */
+	/* recursive call */
+	binary_tree_delete(tree->left);
+	binary_tree_delete(tree->right);
+	free(tree); /* delete each node on return */
 }
