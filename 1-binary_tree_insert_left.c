e0c05ff67c74482ee66b70b7074f33309ff041d0
akanSWE <a.akanSWE@gamil.com>
2024-06-08 04:48:58 +0100

    fix: betty error

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -31,7 +31,5 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 		left_child->left->parent = left_child;
 
 	parent->left = left_child;
-        /* if (left_child->left) */
-	/* 	printf("child new value = %d\n", left_child->left->n); */
 	return (left_child);
 }
