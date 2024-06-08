f33837a81604cb79c62b2d52497a1ea223bc0f31
akanSWE <a.akanSWE@gamil.com>
2024-06-08 10:51:12 +0100

    fix: betty errors.

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -9,8 +9,8 @@
  */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-  if (!node != (node && node->left != node->right))
-	  return (0);
-  else
-	  return (1);
+	if (!node != (node && node->left != node->right))
+		return (0);
+	else
+		return (1);
 }
