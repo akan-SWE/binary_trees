25d7fe079d43ed0eb71f6a3f096f40ddee119d9e
akanSWE <a.akanSWE@gamil.com>
2024-06-08 11:00:27 +0100

    update: code for better readability

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -9,7 +9,7 @@
  */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-	if (!node != (node && node->left != node->right))
+	if (!node || ((node) && (node->left || node->right)))
 		return (0);
 	else
 		return (1);
