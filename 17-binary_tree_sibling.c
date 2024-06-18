8775db3975381785850c1a30b0a7dbe070007da6
akanSWE <a.akanSWE@gamil.com>
2024-06-18 04:53:43 +0100

    fix: Handle the case when NULL is passed to the function.

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -10,10 +10,11 @@
  */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-	binary_tree_t *parent = node->parent;
+	binary_tree_t *parent;
 
 	if (!node || !node->parent)
 		return (NULL);
 	/* If the node is the left child, return the right child, and vice versa */
+	parent = node->parent;
 	return ((parent->left == node) ? parent->right : parent->left);
 }
